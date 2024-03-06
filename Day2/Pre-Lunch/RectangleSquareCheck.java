32c4c640b401b101054d1abfebf95118aa544927
AbdulKareem001 <abdulkareem4613@gmail.com>
2024-03-06 14:58:57 +0530

    Passed

--- Day2/Pre-Lunch/RectangleSquareCheck.java
+++ Day2/Pre-Lunch/RectangleSquareCheck.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
+/**
+ * A program to determine if a rectangle is a square based on the user's input for length and breadth.
+ */
 public class RectangleSquareCheck {
+
+    /**
+     * Main method to execute the rectangle type checking.
+     * Requests user input for length and breadth of a rectangle and determines if it is a square or a rectangle.
+     *
+     * @author Kareem
+     * @since 2024-03-06
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter the length of the rectangle: ");
         double length = input.nextDouble();
+
         System.out.print("Enter the breadth of the rectangle: ");
         double breadth = input.nextDouble();
 
