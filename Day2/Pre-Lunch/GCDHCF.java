0e14b8a00456c398466d63c0f734381acefecc24
AbdulKareem001 <abdulkareem4613@gmail.com>
2024-03-06 13:55:48 +0530

    Passed

---
+++ Day2/Pre-Lunch/GCDHCF.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GCDHCF {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        int result = gcd(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is " + result);
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
