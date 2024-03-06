337089d6b0d4331af2f967a13450fefb10fd566d
AbdulKareem001 <abdulkareem4613@gmail.com>
2024-03-06 16:02:42 +0530

    Passed

---
+++ Day2/Post-Lunch/ArrayOperations.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter integer " + (i + 1) + ": ");
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("The elements in the array are: ");
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
