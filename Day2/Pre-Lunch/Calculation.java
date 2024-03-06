50f79a17f244ab8e2174eb66e072f0e366ed282b
AbdulKareem001 <abdulkareem4613@gmail.com>
2024-03-06 13:30:38 +0530

    Commit

---
+++ Day2/Pre-Lunch/Calculation.java
@@ -0,0 +1,6 @@
+public class Calculation {
+    public static void main(String[] args) {
+        int result = ((((2345 + 8) / 3) % 5) * 5);
+        System.out.println("Final result: " + result);
+    }
+}
